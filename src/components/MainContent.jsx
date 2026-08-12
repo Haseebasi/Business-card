6b51cd62bb200f503ea84b025116a3aa3ce48d70
Haseebasi <haseebasi005@gmail.com>
2026-08-12 12:24:46 +0530

    style applied

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,25 +1,27 @@
 export default function MainContent(){
 return(
   <main>
-  <img src="src\images\moomu.jpg"/>
-  <h2>Moomu</h2>
-  <h3>Frontend Developer</h3>
-  <p>moomu.website</p>
+  <header></header>
+  <h1>Moomu</h1>
+  <span className="job-title">Frontend Developer</span>
+  <span className="web-title">moomu.website</span>
   <div className="Link-divs">
-    <button><i class="fa-solid fa-envelope"></i> Email</button>
-    <button><i class="fa-brands fa-linkedin"></i> Linkedin</button>
+    <button className="email"><i className="fa-solid fa-envelope"></i> Email</button>
+    <button className="linkedin"><i className="fa-brands fa-linkedin"></i> Linkedin</button>
   </div>
   <section>
-    <h3>About</h3>
+    <h2>About</h2>
     <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
-    <h3>Intrests</h3>
+    <h2>Intrests</h2>
     <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
   </section>
   <footer>
+    <div className="footer-items">
     <i class="fa-brands fa-square-twitter"></i>
     <i class="fa-brands fa-square-facebook"></i>
     <i class="fa-brands fa-square-instagram"></i>
     <i class="fa-brands fa-square-github"></i> 
+    </div>
   </footer>
   </main>
 )
